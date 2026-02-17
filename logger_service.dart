5b6b7486f5a0d8d7c4430afb4bcd5b7da24437b0
Nuru <nurusamwel80@gmail.com>
2026-02-17 13:43:36 +0300

    Add files via upload

---
+++ logger_service.dart
@@ -0,0 +1,15 @@
+import 'package:logger/logger.dart';
+
+// 🔹 Singleton logger
+class AppLogger {
+  static final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      methodCount: 0, // show no method info
+      colors: true,
+      printEmojis: true,
+      lineLength: 50,
+    ),
+  );
+
+  static Logger get logger => _logger;
+}
